e0205ae7f6ebe8c96e7287b07a42b72e07484d11
Wesley <WesleyOlivier443@gmail.com>
2026-09-25 17:00:32 +0200

    03 Control toggle

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -40,12 +40,12 @@ const Dashboard = () => {
     const handleJoinMeeting = (e) => {
         e.preventDefault()
         const cleanId = joinId.trim()
-        if (!cleanId) {
+        if (!/^[a-z]{3}(?:-[a-z]{3}){2}$/.text(cleanId)) {
             toast.error("Please enter a valid Meeting ID")
             return
         }
 
-        navigate(`/meeting/${cleanId}`)
+        navigate(`/meeting/${encodeURIComponent(cleanId)}`)
 
     }
 
